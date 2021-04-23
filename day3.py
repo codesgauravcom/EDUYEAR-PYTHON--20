2f44194b16e04f299553c1d9b1fa25d5cd57dda1
gaurav singh <68185584+codesgauravcom@users.noreply.github.com>
2021-04-23 13:14:07 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,16 @@
+#AGE CALCULATOR
+age = input("enter the year of birth ")
+outputage = "{}"
+print(outputage.format(30))
+
+#SIMPLE CALCULATOR
+a,b= 10,5
+print(a+b,a-b,a//b,a/b,a%b,a**b)
+
+#COUNT FUNCTION
+mystr = "python for all"
+print(mystr.count("o"))
+
+#EVEN INDEXED CHARACTERS
+newstr = "eduyearclass"
+print(newstr[::2])
