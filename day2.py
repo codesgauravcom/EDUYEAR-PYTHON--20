66a05352971ee52f8c455a6171247c70fe8fe25c
gaurav singh <68185584+codesgauravcom@users.noreply.github.com>
2021-04-23 12:36:21 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,12 @@
+mystr = "i am learning python from {}"
+print(mystr.format("eduyear"))
+
+mystr2 = "{} for a great course\nGreat presentation  style with lots of opportunities\nThis is my {}nd class"
+print(mystr2.format("THANK YOU!",2))
+
+name = "gaurav"
+age = 21
+erpnumber = 077.6
+print("name is a string data type and its value is {}".format(name))
+print("age is a int data type and its value is {}".format(age))
+print("erpnumber is a float data type and its value is {}".format(erpnumber))
